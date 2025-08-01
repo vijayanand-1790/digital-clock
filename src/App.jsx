ac35104959b6e4b869b3351fa4b49151aca20d26
Vijay Anand <vijayvjrj@gmail.com>
2025-08-01 13:09:43 +0530

    Update: Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
-  const [currentTime,setcurrentTime] = useState(new Date())
+  const [currentTime, setCurrentTime] = useState(new Date())
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setcurrentTime(new Date())  
+      setCurrentTime(new Date())
     }, 1000);
-  
-    return () => clearInterval(timer)
-    
+    return ()=> clearInterval(timer)
   }, [])
 
-  const HourFormatter = (hour)=>{
-    return hour=== 0?12 : hour > 12 ? hour-12 : hour; 
+  const convertTo12 = (hour) => {
+    return hour === 12 ? 12 : hour > 12 ? hour - 12 : hour
   }
 
-  const PreZero = (num)=>{
-    return num<10? "0"+ num : num;
+  const AddZeroinFront = (num) => {
+    return num < 10 ? "0" + num : num;
   }
 
-  // below function (formatDate) seems little different. we have to keep memory of it
-  const formatDate = (date)=>{
-    const option = {weekday: "long", month: "long", year: "numeric", day:"numeric"}
+  const formatDate = (date) => {
+    const option = { weekday: "long", month: "long", year: "numeric", day: "numeric" }
     return date.toLocaleDateString(undefined, option);
   }
 
   return (
     <>
-    <div className="digital-clock">
-      <h2>Digital Clock</h2>
-      <div className="time">{PreZero(HourFormatter(currentTime.getHours()))} : {PreZero(currentTime.getMinutes())} : {PreZero(currentTime.getSeconds())}{currentTime.getHours()>=12 ? " PM":" AM"}</div>
-      <div className="date">{formatDate(currentTime)}</div>
-    </div>
+      <div className='digital-clock'>
+        <h2>Digital Clock</h2>
+        <div className='time'>
+          {AddZeroinFront(convertTo12(currentTime.getHours()))} : {AddZeroinFront(currentTime.getMinutes())} : {AddZeroinFront(currentTime.getSeconds())} <span>{currentTime.getHours() >= 12 ? "PM" : "AM"}</span>
+        </div>
+        <div className='date'> {formatDate(currentTime)} </div>
+      </div>
     </>
   )
 }
